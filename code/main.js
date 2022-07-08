edd3db6f414e14a522ed1faf9e19d15d16839211
Tairone Livinalli <taironelivinalli@hotmail.com>
2022-07-08 23:43:27 +0000

    Added basic game mechanics

--- code/main.js
+++ code/main.js
@@ -5,9 +5,11 @@ kaboom()
 const PLAYER_SPEED = 200
 const INVADER_SPEED_RIGHT = 100
 const INVADER_SPEED_LEFT = -100
+const BULLET_SPEED = 400
 let INVADER_CURRENT_SPEED = INVADER_SPEED_RIGHT
 const STARTER_TIME_LEFT = 100
 const LEVEL_DOWN = 200
+let BULLET_COOLDOWN = false
 
 layer([
   'obj',
@@ -21,18 +23,35 @@ loadSprite('space-ship', '/sprites/space-ship.png')
 loadSprite('invader', '/sprites/invader.png')
 loadSprite('boss', '/sprites/boss.png')
 
+const generateWalls = () => {
+  for (i=0; i < height(); i += 22) {
+    add([
+      sprite('left-wall'),
+      area(),
+      'left-wall',
+      pos(0, i)
+    ])
+    add([
+      sprite('right-wall'),
+      area(),
+      'right-wall',
+      pos(width() - 30, i)
+    ])
+  }
+}
+
 scene('game', () => {
   addLevel([
-  '!^^^^^^^^^^      &',
-  '!^^^^^^^^^^      &',
-  '!^^^^^^^^^^      &',
-  '!                &',
-  '!                &',
-  '!                &',
-  '!                &',
-  '!                &',
-  '!                &',
-  '!                &',
+  ' ^^^^^^^^^^       ',
+  ' ^^^^^^^^^^       ',
+  ' ^^^^^^^^^^       ',
+  '                  ',
+  '                  ',
+  '                  ',
+  '                  ',
+  '                  ',
+  '                  ',
+  '                  ',
   ], {
     width: 30,
     height: 22,
@@ -42,28 +61,54 @@ scene('game', () => {
     '$': () => [sprite('invader'), area()],
     '|': () => [sprite('boss'), scale(2), area()]
   })
+
+  generateWalls()
   
   const player = add([
     sprite('space-ship'),
-    pos(width() / 2, height() / 2),
+    pos(width() / 2, height() - 12),
     origin('center'),
     area(),
   ])
   
   keyDown('w', () => {
+    if (player.pos.y <= 12) return
+    
     player.move(0, -PLAYER_SPEED)
   })
   
   keyDown('s', () => {
+    if (player.pos.y >= height() - 12) return
+    
     player.move(0, PLAYER_SPEED)
   })
   
   keyDown('a', () => {
+    if (player.pos.x <= 45) return
+    
     player.move(-PLAYER_SPEED, 0)
   })
   
   keyDown('d', () => {
+    if (player.pos.x >= width() - 45) return
+    
     player.move(PLAYER_SPEED, 0)
+  })
+
+  keyDown('space', () => {
+    if (BULLET_COOLDOWN) return;
+
+    BULLET_COOLDOWN = true
+    setTimeout(() => BULLET_COOLDOWN = false, 600)
+    
+    add([
+      rect(5, 10),
+      pos(player.pos.x, player.pos.y - 25),
+      origin('center'),
+      color(255, 223, 0),
+      area(),
+      'bullet',
+    ])
   })
   
   const score = add([
@@ -96,6 +141,16 @@ scene('game', () => {
 
   onUpdate('space-invader', (s) => {
     s.move(INVADER_CURRENT_SPEED, 0)
+    
+    if (s.pos.y > height()) go('lose', { score: score.value })
+    if (!get('space-invader').length) 
+      go('win', { score: score.value, time: timer.text })
+  })
+
+  onUpdate('bullet', (b) => {
+    b.move(0, -BULLET_SPEED)
+
+    if (b.pos.y < 0) destroy(b)
   })
 
   collides('space-invader', 'right-wall', () => {
@@ -110,6 +165,14 @@ scene('game', () => {
     every('space-invader', (s) => {
       s.move(0, LEVEL_DOWN)
     })
+  })
+
+  collides('bullet', 'space-invader', (b, s) => {
+    shake(1)
+    destroy(b)
+    destroy(s)
+    score.value += 100
+    score.text = score.value
   })
 
   player.collides('space-invader', () => {
@@ -131,24 +194,52 @@ scene('lose', ({ score }) => {
     scale(1),
     pos(width() / 2, height() / 2)
   ])
-
+  
   add([
-    text('You lose'),
+    text('Press r to restart'),
+    origin('center'),
+    scale(0.5),
+    pos(width() / 2, height() * 0.8)
+  ])
+
+  keyDown('r', () => {
+    go('game')
+  })
+})
+
+scene('win', ({ score, time }) => {
+  add([
+    text('You win'),
     origin('center'),
     scale(1),
     pos(width() / 2, height() / 4)
   ])
   
   add([
-    text('Press space to continue'),
+    text(`${score} points`),
+    origin('center'),
+    scale(0.8),
+    pos(width() / 2, height() / 2 - 20)
+  ])
+  
+
+  add([
+    text(`You had ${time}s left`),
+    origin('center'),
+    scale(0.6),
+    pos(width() / 2, height() / 2 + 50)
+  ])
+  
+  add([
+    text('Press r to restart'),
     origin('center'),
     scale(0.5),
     pos(width() / 2, height() * 0.8)
   ])
 
-  keyDown('space', () => {
+  keyDown('r', () => {
     go('game')
   })
 })
 
-go('game', { score: 0 })
+go('game')
